50838993b7dd7f97afc0224cbb581cc7e77d4e85
JihadDirwanaZaky <jihaddirwanaz@student.telkomuniversity.ac.id>
2025-06-12 05:01:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from supabase import create_client, Client
 
 # ========== Konfigurasi Supabase ==========
 SUPABASE_URL = "https://vmmzsghhyrtddnsmoscw.supabase.co" 
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.xxxxxx"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtbXpzZ2hoeXJ0ZGRuc21vc2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NzYyNTYsImV4cCI6MjA2MzQ1MjI1Nn0.V6G6FTo5hSjYtmGzoHiJz1ez_tcFDhpwkn9qyQlFa0Q"
 
 client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
